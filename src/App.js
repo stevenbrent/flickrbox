2cf424f1cc48a637aac30f7e476faaabfe9b3356
Steven Brent <stevenbrent@users.noreply.github.com>
2017-10-03 16:06:38 -0400

    Up and running!
    Image loader function works - interval set by querystring speed parameter, or defaults to 2000ms. Next up is making the component responsive.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const flickr = "https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=031c640e4395c08b6e604af80abc7669&gallery_id=72157677375993701&format=json&nojsoncallback=1"
+const galleryRequest = "https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=031c640e4395c08b6e604af80abc7669&gallery_id=72157677375993701&format=json&nojsoncallback=1";
+var qSpeed = new URLSearchParams(window.location.search).get("speed");
+var shuffleSpeed =  qSpeed != null ? qSpeed : 2000;
+
+console.log(shuffleSpeed);
+var gallery = [];
+var photoCount;
 
 class App extends Component {
-
+  imageLoader() {
+    var currentImg = gallery[Math.floor(Math.random() * (photoCount - 1))];
+    console.log('loading image');
+    var imgel = document.getElementById("dynImg");
+    imgel.src = currentImg;
+  }
   componentDidMount() {
-    fetch(flickr)
+    fetch(galleryRequest)
       .then((response) => {
         return response.json()
       })
       .then((body) => {
-          var photoCount = body.photos.total;
-          console.log(`Total Photos: ${photoCount}`);
+        body.photos.photo.forEach((photo) => {
+          var id = photo.id;
+          var secret = photo.secret;
+          var server = photo.server;
+          var farm = photo.farm;
+
+          var photoURL = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`;
+          gallery.push(photoURL);
+        })
+        photoCount = gallery.length;
+        window.addEventListener('click', setInterval(this.imageLoader, shuffleSpeed));
       });
   }
-
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img id='dynImg' src='' alt='cute'></img>
       </div>
     );
   }
 }
-
 export default App;
